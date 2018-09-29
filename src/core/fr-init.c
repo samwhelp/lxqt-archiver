b6740c15240141ff3a2231119ccb24e3a8fb711c
samwhelp <samwhelp@users.noreply.github.com>
2018-09-29 22:45:24 +0800

    force-use-unar v1

--- src/core/fr-init.c
+++ src/core/fr-init.c
@@ -374,6 +374,24 @@ get_command_type_from_mime_type (const char    *mime_type,
 	if (mime_type == NULL)
 		return 0;
 
+	if (g_ascii_strcasecmp(mime_type, "application/zip") == 0) { // only for zip
+		if (requested_capabilities == FR_COMMAND_CAN_READ_WRITE) { // first call
+			return 0;
+		} else if (requested_capabilities == FR_COMMAND_CAN_READ) { // second call
+			FrRegisteredCommand *command;
+			//FrCommandCaps        capabilities;
+			command = g_ptr_array_index (Registered_Commands, Registered_Commands->len-1); // last command -> FR_TYPE_COMMAND_UNARCHIVER
+			//capabilities = fr_registered_command_get_capabilities (command, mime_type);
+
+			debug (DEBUG_INFO, " command->type: %d ", command->type);
+
+			if (command->type == FR_TYPE_COMMAND_UNARCHIVER) { // force use unar
+				return command->type;
+			}
+
+		}
+	}
+
 	for (i = 0; i < Registered_Commands->len; i++) {
 		FrRegisteredCommand *command;
 		FrCommandCaps        capabilities;
